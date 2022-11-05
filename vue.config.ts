bb267c4052178eef83ef50701bef785984a85d8f
Mohamed Amine Fh <fhal.mohamed.amine@gmail.com>
2022-11-05 20:11:50 +0100

    add eslint

--- vue.config.ts
+++ vue.config.ts
@@ -1,11 +1,3 @@
 module.exports = {
-  publicPath: "/unodit_client/",
-  pluginOptions: {
-    i18n: {
-      locale: "en",
-      fallbackLocale: "en",
-      localeDir: "locales",
-      enableInSFC: false,
-    },
-  },
+  publicPath: process.env.NODE_ENV === "production" ? "/portfolio/" : "/",
 };
