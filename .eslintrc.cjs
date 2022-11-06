2246975032b80a42cfc7b197a8c34e40ce98e672
Mohamed Amine Fh <fhal.mohamed.amine@gmail.com>
2022-11-06 17:08:24 +0100

    add dummy component

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,19 +12,4 @@ module.exports = {
   env: {
     "vue/setup-compiler-macros": true,
   },
-  rules: {
-    "@typescript-eslint/no-explicit-any": "off",
-    "no-unused-vars": 0,
-    "@typescript-eslint/no-non-null-assertion": "off",
-    "@typescript-eslint/no-unused-vars": 0,
-    "@typescript-eslint/no-empty-function": "off",
-    "@typescript-eslint/no-non-null-asserted-optional-chain": "off",
-    "vue/require-default-prop": "off",
-    "vue/multi-word-component-names": "off",
-    //'no-unused-variable': 0,
-
-    // 'no-unused-declaration': 0,
-  },
-  parser: "vue-eslint-parser",
-  plugins: ["vue"],
 };
