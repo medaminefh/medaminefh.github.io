9b8a93d60226e5d7b94c784dd6c7188142a26504
Mohamed Amine Fh <fhal.mohamed.amine@gmail.com>
2023-03-09 21:34:17 +0100

    delete for dark mode

--- src/components/contact.vue
+++ src/components/contact.vue
@@ -46,14 +46,14 @@ const submit = async () => {
       <div class="mb-6">
         <label
           for="subject"
-          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          class="block mb-2 text-sm font-medium text-gray-900"
           >Subject</label
         >
         <input
           type="text"
           id="subject"
           v-model="state.subject"
-          class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+          class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
           placeholder="Subject..."
           required
         />
@@ -61,21 +61,21 @@ const submit = async () => {
       <div class="mb-6">
         <label
           for="message"
-          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          class="block mb-2 text-sm font-medium text-gray-900"
           >Your message</label
         >
         <textarea
           id="message"
           rows="4"
           v-model="state.body"
-          class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
           placeholder="Send a message..."
         ></textarea>
       </div>
       <button
         type="submit"
+        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
         :class="loading && 'bg-blue-400 cursor-not-allowed pointer-events-none'"
-        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       >
         {{ loading ? "pending..." : "Send" }}
       </button>
